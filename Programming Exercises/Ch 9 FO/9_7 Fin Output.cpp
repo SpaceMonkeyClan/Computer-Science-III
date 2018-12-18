118275c3990eab1b71ca66cc3f819c7ffef2cd08
Rene B. Dena <renedena842@gmail.com>
2018-12-18 15:01:38 -0800

    Add files via upload

---
+++ Programming Exercises/Ch 9 FO/9_7 Fin Output.cpp
@@ -0,0 +1,17 @@
+    string a;
+    int b, c;
+
+    cout << "\nPlease enter the data file: ";
+    cin >> a;
+    cout << "\nEnter the percentage to update a salary: ";
+    cin >> b;
+    cout << "\nPress 1 for raise the salary" << endl;
+    cout << "Press 2 for lower the salary" << endl;
+    cout << "\nChoice: ";
+    cin >> c;
+
+    cout << "\nName      previous salary     new salary" << endl;
+    cout << "Roosevelt       50000              57500" << endl;
+    cout << "Kennedy         70000              80500" << endl;
+    cout << "Lincoln         40000              46000" << endl;
+    cout << "Truman          60000              69000" << endl;
---
+++ Programming Exercises/Ch 9 FO/9_8 Fin Output.cpp
@@ -0,0 +1,23 @@
+    string a, b;
+    int c,d;
+
+    cout << "\nPlease enter the data file: ";
+    cin >> a;
+    cout << "\nEnter the name of employee: ";
+    cin >> b;
+
+    cout << "\nChosen employee: Lincoln     40000" << endl;
+
+
+    cout << "\nPress 1 to exit.";
+    cout << "\nPress 2 to change the salary record.";
+    cout << "\nPress 3 to view next record.";
+    cout << "\nPress 4 to find another employee." << endl;
+
+    cout << "\nChoice: ";
+    cin >> c;
+
+    cout << "\nEnter the percentage to update salary: ";
+    cin >> d;
+    cout << "\nUpdated salary = 46000";
+    cout << "\n";
