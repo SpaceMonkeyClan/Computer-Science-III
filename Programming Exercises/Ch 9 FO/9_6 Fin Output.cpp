c0a112cf624366111a4358dd0851bd1917ca21d3
Rene B. Dena <renedena842@gmail.com>
2018-12-18 15:01:19 -0800

    Create 9_6 Fin Output.cpp

---
+++ Programming Exercises/Ch 9 FO/9_6 Fin Output.cpp
@@ -0,0 +1,36 @@
+cout << "\nDisplaying contents of database" << endl;
+cout << "Mr.| Harry| Hacker | 1105 Torre Ave.| Cupertino | CA | 95014\n"
+        "Dr.| John | Lee | 702 Ninth Street Apt. 4 | San Jose | CA | 95109\n"
+        "Miss | Evelyn | Garcia | 1101 S. University Place | Ann Arbor | MI | 48105" << endl;
+
+cout << "\nDisplaying contents of template" << endl;
+cout << "To:\n"
+        "|1| |2| |3|\n"
+        "|4|\n"
+        "|5|, |6| |7|\n"
+        "Dear |1| |3|:\n"
+        "You and the |3| family may be the lucky winners of $10,000,000 in the C++ compiler clearinghouse sweepstakes! ..." << endl;
+
+cout << "\nJunk letter for Record 0" << endl;
+cout << "To: " << endl;
+cout << "Mr. Harry Hacker" << endl;
+cout << "1105 Torre Ave." << endl;
+cout << "Cupertino, CA 95014" << endl;
+cout << "Dear Mr. Hacker: " << endl;
+cout << "You and the Hacker family may be the lucky winners of $10,000,000 in the C++ compiler clearinghouse sweepstakes! ..." << endl;
+
+cout << "\nJunk letter for Record 1" << endl;
+cout << "To: " << endl;
+cout << "Dr. John Lee" << endl;
+cout << "702 Ninth Street APt. 4" << endl;
+cout << "San Jose, CA 95109" << endl;
+cout << "Dear Dr. Lee: " << endl;
+cout << "You and the Lee family may be the lucky winners of $10,000,000 in the C++ compiler clearinghouse sweepstakes! ..." << endl;
+
+cout << "\nJunk letter for Record 2" << endl;
+cout << "To: " << endl;
+cout << "Miss Evelyn Garcia" << endl;
+cout << "1101 S. University Place" << endl;
+cout << "Ann Arbor, MI 48105" << endl;
+cout << "Dear Miss Garcia: " << endl;
+cout << "You and the Garcia family may be the lucky winners of $10,000,000 in the C++ compiler clearinghouse sweepstakes! ..." << endl;
